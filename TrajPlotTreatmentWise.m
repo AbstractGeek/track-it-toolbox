ba2ab4e0d21b5ae87150e911a1e64128130dd13d
Dinesh Natesan <AbstractGeek@users.noreply.github.com>
2017-10-12 11:29:01 +0530

    Added treatment wise trajectory plotting code

---
+++ TrajPlotTreatmentWise.m
@@ -0,0 +1,54 @@
+function [] = TrajPlotTreatmentWise(sorted_matfile)
+% 
+% 
+
+% Load data etc.
+data = load(sorted_matfile);
+rootdir = fileparts(sorted_matfile);
+treatments = fieldnames(data);
+
+% run through each treatment
+for i=1:length(treatments)
+    
+    treatmentData = struct;
+    days = fieldnames(data.(treatments{i}));   
+    days(ismember(days,{'name'})) = [];
+    
+    for j=1:length(days)
+        % run through each day                 
+        curr_day = days{j};
+        flies = fieldnames(data.(treatments{i}).(curr_day));
+        
+        if (j==1)
+            % obtain center
+            cs = reshape(data.(treatments{i}).(days{1}).(flies{1}){1,5:end}, 3, [])';
+            % Make it centered to the first object
+            cs = cs - repmat(cs(1,:),size(cs,1),1);            
+        end
+
+        for k=1:length(flies)            
+            % add fly data to treatmentData after subtracting centers
+            treatmentData.(sprintf('%s_%s', curr_day, flies{k})).X = ...
+                data.(treatments{i}).(days{j}).(flies{k}).X - ...
+                data.(treatments{i}).(days{j}).(flies{k}).cs01_X(1);
+            
+            treatmentData.(sprintf('%s_%s', curr_day, flies{k})).Y = ...
+                data.(treatments{i}).(days{j}).(flies{k}).Y - ...
+                data.(treatments{i}).(days{j}).(flies{k}).cs01_Y(1);
+
+            treatmentData.(sprintf('%s_%s', curr_day, flies{k})).Z = ...
+                data.(treatments{i}).(days{j}).(flies{k}).Z - ...
+                data.(treatments{i}).(days{j}).(flies{k}).cs01_Z(1);            
+        end
+    
+    end
+    
+    % Plot data for whole of the treatment
+    TrajPlotDayWise(cs,treatments{i},treatmentData,...
+        fullfile(rootdir,sprintf('%s.fig',treatments{i})));
+    
+    
+end
+
+
+end
