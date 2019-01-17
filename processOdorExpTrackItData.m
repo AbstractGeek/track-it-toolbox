02b5d675c22ee00742384997c49e4a65d57f4740
Dinesh Natesan <AbstractGeek@users.noreply.github.com>
2019-01-17 18:43:02 +0530

    fixing gui input to the processOdorExpTrackItData.m

--- processOdorExpTrackItData.m
+++ processOdorExpTrackItData.m
@@ -8,20 +8,14 @@
 force_rewrite = 1;
 backup_on = 0;
 
-%% Experiment folders (hardcoded)
-expDir = '/media/dinesh/M2SSD/Odor Concentration Data/Airflow';
-outDir = '/media/dinesh/M2SSD/Odor Concentration Analysis/Airflow/Output';
-backupDir = '/media/dinesh/M2SSD/Odor Concentration Analysis/Airflow/Backup';
-save_folder = '/media/dinesh/M2SSD/Odor Concentration Analysis/Airflow/Sorted';
-
-% %% Get the experiment folder, output folder and backup folder
-% expDir = uigetdir(pwd,'Experiment Folder (Trackit save folder)');
-% rootDir = fileparts(expDir);
-% outDir = uigetdir(rootDir,'Output Folder (Main Data folder)');
-% rootDir = fileparts(outDir);
-% backupDir = uigetdir(rootDir,'Backup Folder (To move data)');
-% rootDir = fileparts(backupDir);
-% save_folder = uigetdir(rootDir,'Sorted data folder (sorted data)');
+%% Get the experiment folder, output folder and backup folder
+expDir = uigetdir(pwd,'Experiment Folder (Trackit save folder)');
+rootDir = fileparts(expDir);
+outDir = uigetdir(rootDir,'Output Folder (Main Data folder)');
+rootDir = fileparts(outDir);
+backupDir = uigetdir(rootDir,'Backup Folder (To move data)');
+rootDir = fileparts(backupDir);
+save_folder = uigetdir(rootDir,'Sorted data folder (sorted data)');
 
 %% Process trackit data
 raw_data_matfile = copyTrackitExperimentData(expDir,outDir,backupDir,backup_on,force_rewrite);
